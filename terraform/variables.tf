49f17f3b9263c1066cfff044cec70fd71442feca
jenjen2424 <jennifer.ma@betterage.net>
2025-10-04 07:29:28 -0400

    edit variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,11 @@
 variable "project_id" {
   description = "The ID of the project in which to create the resources."
   type        = string
-  default     = "wired-epsilon-472120-j8"
 }
 
 variable "bucket_name" {
   description = "The name of the Google Cloud Storage bucket."
   type        = string
-  default     = "wired-epsilon-472120-j8-nyc-taxi-bucket"
 }
 
 variable "dataset_name" {
